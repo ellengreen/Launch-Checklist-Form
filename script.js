9cc4803c2b275e12196a14618eb39f728663a3f3
Ellen Green <eegreen.95@gmail.com>
2019-11-18 22:00:46 -0600

    Fetched JSON data

--- script.js
+++ script.js
@@ -57,5 +57,20 @@ window.addEventListener('load', function() {
       }
     }
 
-   })
+  })
+  fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json) {
+         const destination = document.getElementById("missionTarget");
+            destination.innerHTML = `<h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[0].name}</li>
+               <li>Diameter: ${json[0].diameter}</li>
+               <li>Star: ${json[0].star}</li>
+               <li>Distance from Earth: ${json[0].distance}</li>
+               <li>Number of Moons: ${json[0].moons}</li>
+            </ol>
+               <img src="${json[0].image}">
+            `;
+  })
+})
 })
