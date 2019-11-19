fcab4ca3e82facf5b6625ea063dfdb9502b1ab99
Ellen Green <eegreen.95@gmail.com>
2019-11-18 21:38:34 -0600

    added validation so all fields are required & input must be correct type

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Write your JavaScript code here!
 window.addEventListener('load', function() {
-   let form = document.querySelector('form');
-   let pilot = document.querySelector("input[name=pilotName]");
-   let coPilot = document.querySelector("input[name=copilotName]");
-   let fuel = document.querySelector("input[name=fuelLevel]");
-   let cargo = document.querySelector("input[name=cargoMass]");
+  let form = document.querySelector('form');
+  let pilot = document.querySelector("input[name=pilotName]");
+  let coPilot = document.querySelector("input[name=copilotName]");
+  let fuel = document.querySelector("input[name=fuelLevel]");
+  let cargo = document.querySelector("input[name=cargoMass]");
 
-   let pilotStatus = document.getElementById('pilotStatus');
-   let copilotStatus = document.getElementById('copilotStatus');
-   let fuelStatus = document.getElementById('fuelStatus');
-   let cargoStatus = document.getElementById('cargoStatus');
-   let faultyItems = document.getElementById('faultyItems');
-   let launchStatus = document.getElementById('launchStatus');
+  let pilotStatus = document.getElementById('pilotStatus');
+  let copilotStatus = document.getElementById('copilotStatus');
+  let fuelStatus = document.getElementById('fuelStatus');
+  let cargoStatus = document.getElementById('cargoStatus');
+  let faultyItems = document.getElementById('faultyItems');
+  let launchStatus = document.getElementById('launchStatus');
 
-   form.addEventListener('submit', function() {
+  form.addEventListener('submit', function() {
+
+    if (pilot.value === "" || coPilot.value === "" || fuel.value === "" || cargo.value === "") {
+      alert('All fields are required!');
+      event.preventDefault();
+    } else if (isNaN(fuel.value) || isNaN(cargo.value) || !isNaN(pilot.value) || !isNaN(coPilot.value)) {
+        alert('Must enter valid information for each field!');
+        event.preventDefault();
+    };
+
+
 
    })
 })
